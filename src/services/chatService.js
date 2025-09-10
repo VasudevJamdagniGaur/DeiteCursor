71c87a42a6713fafb659fce0b154c807a942f1e4
VasudevJamdagniGaur <vasudevjamdagnigaur@gmail.com>
2025-09-10 19:25:44 +0530

    Update base URL in ChatService to new endpoint for improved connectivity.

--- src/services/chatService.js
+++ src/services/chatService.js
@@ -1,6 +1,6 @@
 class ChatService {
   constructor() {
-    this.baseURL = 'https://4wyil877gtsxl4-11434.proxy.runpod.net/';
+    this.baseURL = 'https://es7dfgaw5nb3k6-11434.proxy.runpod.net/';
   }
 
   async sendMessage(userMessage, conversationHistory = []) {
