0964d8fc85e9cf6d698e9b86598eede7ccef4d00
VasudevJamdagniGaur <vasudevjamdagnigaur@gmail.com>
2025-10-02 13:45:29 +0530

    Update base URL in PatternAnalysisService to new API endpoint for improved connectivity

--- src/services/patternAnalysisService.js
+++ src/services/patternAnalysisService.js
@@ -3,7 +3,7 @@ import { getDateIdDaysAgo, getDateId } from '../utils/dateUtils';
 
 class PatternAnalysisService {
   constructor() {
-    this.baseURL = 'https://hqoin4jvwz6kq1-11434.proxy.runpod.net/';
+    this.baseURL = 'https://43fjynx0rc51ps-11434.proxy.runpod.net/';
     this.minDaysRequired = 3; // Minimum days needed for meaningful analysis
     this.minMessagesRequired = 8; // Minimum total messages needed
     this.minDaysFor3Months = 7; // Minimum days for 3-month analysis
