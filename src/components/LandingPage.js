515dd23cf78f0a4a063bb099ad918be49a978224
VasudevJamdagniGaur <vasudevjamdagnigaur@gmail.com>
2025-10-08 00:24:12 +0530

    Update navigation in LandingPage to redirect to signup instead of welcome

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,7 +6,7 @@ export default function LandingPage() {
   const navigate = useNavigate();
 
   const handleGetStarted = () => {
-    navigate('/welcome');
+    navigate('/signup');
   };
 
   return (
