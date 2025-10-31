472244c86bcc59db8d255bfe2d440f8309aee4a2
VasudevJamdagniGaur <vasudevjamdagnigaur@gmail.com>
2025-10-31 12:49:15 +0530

    Add initial Android project structure and Capacitor configuration
    - Created a new Capacitor configuration file to define app settings for the Android platform.
    - Added necessary dependencies for Capacitor and Firebase authentication in package.json and package-lock.json.
    - Established the Android project structure with essential Gradle files, settings, and configurations.
    - Included initial AndroidManifest.xml and MainActivity.java for app entry point and configuration.
    - Added drawable resources for app icons and splash screens to enhance visual presentation.
    - Implemented basic layout for the main activity using a CoordinatorLayout and WebView.
    - Set up ProGuard rules and testing files for improved build and testing processes.

---
+++ android/app/src/main/java/com/deite/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.deite.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'jamdagni.deite.app',
+  appName: 'Deite : AI Journel',
+  webDir: 'build'
+};
+
+export default config;
