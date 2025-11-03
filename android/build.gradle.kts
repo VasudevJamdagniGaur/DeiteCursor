f06256c4a5ed87730206124227f8e3bae978bcbe
VasudevJamdagniGaur <vasudevjamdagnigaur@gmail.com>
2025-11-03 18:09:46 +0530

    Add Google services Gradle plugin to build.gradle.kts for Firebase integration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
+plugins {
+    id("com.google.gms.google-services") version "4.4.4" apply false
+}
+
 buildscript {
     repositories {
         google()
@@ -7,9 +11,6 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.2")
-        // Add the dependency for the Google services Gradle plugin
-        classpath("com.google.gms:google-services:4.4.4")
-        
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
