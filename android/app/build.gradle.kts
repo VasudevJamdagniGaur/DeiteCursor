f99efaf1f37a4eff167e84dc850c47b3df1ca2e5
VasudevJamdagniGaur <vasudevjamdagnigaur@gmail.com>
2025-11-12 14:23:51 +0530

    Update versionCode and versionName in build.gradle.kts for app release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "therapist.deite.app"
         minSdk = rootProject.ext["minSdkVersion"] as Int
         targetSdk = rootProject.ext["targetSdkVersion"] as Int
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
         aaptOptions {
