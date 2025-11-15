3c04cb985f1f47aae883287d474ba5de1a860e7f
VasudevJamdagniGaur <vasudevjamdagnigaur@gmail.com>
2025-11-15 17:07:50 +0530

    Update version code and name in build.gradle.kts to reflect the new release (1.0.2)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "therapist.deite.app"
         minSdk = rootProject.ext["minSdkVersion"] as Int
         targetSdk = rootProject.ext["targetSdkVersion"] as Int
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
         aaptOptions {
